6a2e2eb7d4e2cc6ed5f149229e555756a0369878
Villette Comfort <villette@live.com>
2022-08-01 13:30:57 -0700

    Added models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {model: Product}
+    ]
+  })
+  .then(categoryData => {
+    res.json(categoryData)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    
+  })
 });
 
 router.post('/', (req, res) => {
