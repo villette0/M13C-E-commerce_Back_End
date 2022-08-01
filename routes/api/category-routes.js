bbf9c286e3c34b8469c8dfb229d3619f21ede3ba
Villette Comfort <villette@live.com>
2022-08-01 15:25:14 -0700

    Adding product routs

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,14 +40,43 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(categoryData => {
+    res.json(categoryData)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+  }
+  })
+  .then(categoryData => {
+    res.json(categoryData)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    res.json(categoryData)
+})
+.catch(err => {
+  res.json(err)
+})
 });
 
 module.exports = router;
