57f3d66a663a21edcf00197e10ad9a2cade6d8ae
Villette Comfort <villette@live.com>
2022-08-01 18:28:37 -0700

    Working on product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,6 +25,21 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {model: Category},
+      {model: Tag}
+    ]
+  })
+  .then(productData => {
+    res.json(productData)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 });
 
 // create new product
