b9366543e4818a392826822a7685b82f0d6387e1
Villette Comfort <villette@live.com>
2022-08-02 16:01:40 -0700

    Finished adding routes for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,6 +118,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    res.json(categoryData)
+})
+.catch(err => {
+  res.json(err)
+})
 });
 
 module.exports = router;
